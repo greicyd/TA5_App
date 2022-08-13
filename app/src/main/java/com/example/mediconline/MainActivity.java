4b272facbbf2ef201681fca3b01ee1fd03e2a223
greicyd <93390343+greicyd@users.noreply.github.com>
2022-08-13 08:31:19 -0500

    Pantalla Spash

--- app/src/main/java/com/example/mediconline/MainActivity.java
+++ app/src/main/java/com/example/mediconline/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mediconline;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimerTask tarea= new TimerTask() {
+            @Override
+            public void run() {
+                Intent intend=new Intent(MainActivity.this, PrincipalActivity.class);
+                startActivity(intend);
+                finish();
+            }
+
+        };
+        Timer tiempo =new Timer();
+        tiempo.schedule(tarea,3000);
     }
 }
